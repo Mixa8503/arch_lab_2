85ac88a1f9957e62d9095cfa2595c5c41adcc7f2
sanixto <sanix.to@gmail.com>
2022-06-28 01:59:34 +0300

    added implementation test

--- implementation.go
+++ implementation.go
@@ -1,88 +1,88 @@
 package lab2
 
 import (
-  "fmt"
-  "math"
-  "strconv"
-  "strings"
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
 )
 
 type Stack []float64
 
 func (s *Stack) push(item float64) {
-  *s = append(*s, item)
+	*s = append(*s, item)
 }
 
 func (s *Stack) pop() (float64, error) {
-  l := len(*s)
-  if l == 0 {
-    return 0.0, fmt.Errorf("Stack is empty")
-  }
-  item := (*s)[l-1]
-  *s = (*s)[:l-1]
-  return item, nil
+	l := len(*s)
+	if l == 0 {
+		return 0.0, fmt.Errorf("Stack is empty")
+	}
+	item := (*s)[l-1]
+	*s = (*s)[:l-1]
+	return item, nil
 }
 
 func isOperator(str string) bool {
-  operators := [...]string{"+", "-", "*", "/", "^"}
-  for _, operator := range operators {
-    if str == operator {
-      return true
-    }
-  }
-  return false
+	operators := [...]string{"+", "-", "*", "/", "^"}
+	for _, operator := range operators {
+		if str == operator {
+			return true
+		}
+	}
+	return false
 }
 
 func Prefix_Val(expr []string) bool {
-  if !isOperator(expr[0]) {
-    return false
-  }
-  var Operator_counter, Operand_counter int
-  for _, str := range expr {
-    if isOperator(str) {
-    Operator_counter++
-      continue
-    }
-    Operand_counter++
-  }
-  if Operand_counter == Operator_counter + 1 {
-    return true
-  }
-  return false
+	if !isOperator(expr[0]) {
+		return false
+	}
+	var Operator_counter, Operand_counter int
+	for _, str := range expr {
+		if isOperator(str) {
+			Operator_counter++
+			continue
+		}
+		Operand_counter++
+	}
+	if Operand_counter == Operator_counter+1 {
+		return true
+	}
+	return false
 }
 
 func Prefix_Eval(expr string) (float64, error) {
-  var stack Stack
-  values := strings.Fields(expr)
-  res := Prefix_Val(values)
-  if !res {
-    return 0.0, fmt.Errorf("incorrect input")
-  }
-  for i := len(values) - 1; i >= 0; i-- {
-    value := values[i]
-    if num, err := strconv.ParseFloat(value, 64); err == nil {
-      stack.push(num)
-    } else {
-      operand1, err1 := stack.pop()
-      operand2, err2 := stack.pop()
-      if err1 != nil || err2 != nil {
-        return 0.0, fmt.Errorf("Something is wrong, you need to check input")
-      }
-      switch value {
-      case "+":
-        stack.push(operand1 + operand2)
-      case "-":
-        stack.push(operand1 - operand2)
-      case "*":
-        stack.push(operand1 * operand2)
-      case "/":
-        stack.push(operand1 / operand2)
-      case "^":
-        stack.push(math.Pow(operand1, operand2))
-      default:
-        return 0.0, fmt.Errorf("Choose operators: { +, -, *, /, ^ }")
-      }
-    }
-  }
-  return stack.pop()
+	var stack Stack
+	values := strings.Fields(expr)
+	res := Prefix_Val(values)
+	if !res {
+		return 0.0, fmt.Errorf("incorrect input")
+	}
+	for i := len(values) - 1; i >= 0; i-- {
+		value := values[i]
+		if num, err := strconv.ParseFloat(value, 64); err == nil {
+			stack.push(num)
+		} else {
+			operand1, err1 := stack.pop()
+			operand2, err2 := stack.pop()
+			if err1 != nil || err2 != nil {
+				return 0.0, fmt.Errorf("Something is wrong, you need to check input")
+			}
+			switch value {
+			case "+":
+				stack.push(operand1 + operand2)
+			case "-":
+				stack.push(operand1 - operand2)
+			case "*":
+				stack.push(operand1 * operand2)
+			case "/":
+				stack.push(operand1 / operand2)
+			case "^":
+				stack.push(math.Pow(operand1, operand2))
+			default:
+				return 0.0, fmt.Errorf("Choose operators: { +, -, *, /, ^ }")
+			}
+		}
+	}
+	return stack.pop()
 }
--- implementation_test.go
+++ implementation_test.go
@@ -7,17 +7,67 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPrefixToPostfix(t *testing.T) {
-	res, err := PrefixToPostfix("+ 5 * - 4 2 3")
+func TestPrefixEvaluate(t *testing.T) {
+	res, err := Prefix_Eval("+ 3 4")
 	if assert.Nil(t, err) {
-		assert.Equal(t, "4 2 - 3 * 5 +", res)
+		assert.Equal(t, 7.0, res)
 	}
-}
 
-func ExamplePrefixToPostfix() {
-	res, _ := PrefixToPostfix("+ 2 2")
+	res, err = Prefix_Eval("- 4 2")
+	if assert.Nil(t, err) {
+		assert.Equal(t, 2.0, res)
+	}
+
+	res, err = Prefix_Eval("* 8 9")
+	if assert.Nil(t, err) {
+		assert.Equal(t, 72.0, res)
+	}
+
+	res, err = Prefix_Eval("/ 64 4")
+	if assert.Nil(t, err) {
+		assert.Equal(t, 16.0, res)
+	}
+
+	res, err = Prefix_Eval("^ 2 5")
+	if assert.Nil(t, err) {
+		assert.Equal(t, 32.0, res)
+	}
+
+	res, err = Prefix_Eval("^ + 2 5 2")
+	if assert.Nil(t, err) {
+		assert.Equal(t, 49.0, res)
+	}
+
+	res, err = Prefix_Eval("- * 2 17 2")
+	if assert.Nil(t, err) {
+		assert.Equal(t, 32.0, res)
+	}
+
+	res, err = Prefix_Eval("- * / 2 16 2 - 8 2")
+	if assert.Nil(t, err) {
+		assert.Equal(t, -5.75, res)
+	}
+
+	res, err = Prefix_Eval("f")
+	assert.Equal(t, 0.0, res)
+	assert.Equal(t, "incorrect input", err.Error())
+
+	res, err = Prefix_Eval("++ 4 2")
+	assert.Equal(t, 0.0, res)
+	assert.Equal(t, "incorrect input", err.Error())
+
+	res, err = Prefix_Eval("+ 4 2 - 2 4 9")
+	assert.Equal(t, 0.0, res)
+	assert.Equal(t, "incorrect input", err.Error())
+
+	res, err = Prefix_Eval("+ - 4 2 3 ^ 2")
+	assert.Equal(t, 0.0, res)
+	assert.Equal(t, "Something is wrong, you need to check input", err.Error())
+}
+func ExamplePrefixEvaluate() {
+	res, _ := Prefix_Eval("+ 10 3")
 	fmt.Println(res)
 
 	// Output:
-	// 2 2 +
+	// 13
 }
